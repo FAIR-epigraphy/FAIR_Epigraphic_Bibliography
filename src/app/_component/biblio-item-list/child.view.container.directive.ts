d8b5b97bec20243c3573bb52ee96191835e6f098
imranasif87 <56230659+imranasif87@users.noreply.github.com>
2023-05-03 15:03:24 +0000

    Deploying to main from  @ 429883d19e8b0747b943c70d236fcf8adab6345c 🚀

--- src/app/_component/biblio-item-list/child.view.container.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-  selector: '[childViewContainer]',
-})
-export class childViewContainerDirective {
-  constructor(public viewContainerRef: ViewContainerRef) { }
-}
